5e30dec05ae48f7fec10233b58ffe7432f83ad3d
L3nn0x <dragon83.super@gmail.com>
2019-09-15 12:18:27 +0100

    Added source code

--- Source/Rose/RoseGameModeBase.cpp
+++ Source/Rose/RoseGameModeBase.cpp
@@ -3,3 +3,25 @@
 
 #include "RoseGameModeBase.h"
 
+void ARoseGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+}
+
+void ARoseGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
